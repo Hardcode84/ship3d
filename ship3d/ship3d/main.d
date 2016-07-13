b91d7205db5a3436d323d74f924855fa9dc89493
Ivan <ivan.butygin@gmail.com>
2016-07-13 19:04:37 +0300

    custom assert handler

--- ship3d/ship3d/main.d
+++ ship3d/ship3d/main.d
@@ -2,6 +2,17 @@
 
 import game.game;
 
+extern(C)
+{
+    void _d_assert(string file, uint line)
+    {
+        import std.stdio;
+        writefln("Assertion in \"%s\" on line %s", file, line);
+        import core.stdc.stdlib;
+        abort();
+    }
+}
+
 void main(string[] args)
 {
     scope game = new Game(args);
